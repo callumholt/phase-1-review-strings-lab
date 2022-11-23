d7bb2e36277290393b445c254a68ac00d8125d4d
callum <callumpholt@gmail.com>
2022-11-24 08:34:00 +1100

    all tests past

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+console.log(shortGreeting);
